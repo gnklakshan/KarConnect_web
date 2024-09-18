f4abe562acdba53c13c1f19c76b87eed4b7cf60a
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-09-18 22:57:25 +0530

    remove monthly earning

--- src/app/manage/booking/page.tsx
+++ src/app/manage/booking/page.tsx
@@ -351,7 +351,7 @@ const BookingPage: React.FC = () => {
                 <div>Canceled Requests</div>
               </div>
               <div className="flex-1 mb-4 md:mb-0">
-                <div className="text-2xl font-bold text-primary-color">Rs.{monthlyEarnings.toFixed(2)}</div>
+                <div className="text-2xl font-bold text-primary-color">Rs.{}</div>
                 <div>Monthly Earnings</div>
               </div>
             </div>
