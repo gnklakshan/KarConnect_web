7d51290e48056b46c209cffea5020e0c874a04be
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2024-10-21 22:39:44 +0530

    set env values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -411,7 +411,6 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import LandingPage from "./landing/page";
 
-
 const HomePage = () => {
   const router = useRouter();
 
@@ -420,16 +419,16 @@ const HomePage = () => {
   };
 
   return (
-   // <div className="bg-gray-100 flex min-h-screen flex-col items-center justify-center">
+    // <div className="bg-gray-100 flex min-h-screen flex-col items-center justify-center">
     //  <h1 className="mb-8 text-4xl font-bold">Welcome to KarConnect</h1>
     //  <button
     //    onClick={handleSignIn}
-     //   className="rounded-lg bg-blue-500 px-6 py-3 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
-     // >
-      //  Go to Sign In
+    //   className="rounded-lg bg-blue-500 px-6 py-3 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+    // >
+    //  Go to Sign In
     //  </button>
-   // </div>
-   <LandingPage/>
+    // </div>
+    <LandingPage />
   );
 };
 
